9151df921a8f590f8349f3a6e6f3b4dc5ffb3194
FRFlo <62435410+FRFlo@users.noreply.github.com>
2022-04-07 22:40:09 +0200

    Cleanup code

--- script.js
+++ script.js
@@ -3,13 +3,3 @@ window.addEventListener('load', function() {
         document.getElementById('loading').style.display = 'none';
     }
 })
-
-window.onscroll = function(){
-if (($(window).width() >= 900)){
-    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        $("#middle").css("background-size","150% auto");
-    }else{
-        $("#middle").css("background-size","100% auto");        
-    }
-}
-};
